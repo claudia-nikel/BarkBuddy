b118e8c1acde1edfa28a97838115a84369dc27c2
claudia-nikel <claudia.nikel@gmail.com>
2024-06-26 13:18:56 -0700

    Add more fields for dog info

--- src/components/AddDog.js
+++ src/components/AddDog.js
@@ -7,7 +7,11 @@ import './AddDog.css';
 
 const AddDog = () => {
   const [name, setName] = useState('');
-  const [info, setInfo] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [color, setColor] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [owner, setOwner] = useState('');
   const [image, setImage] = useState(null);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -15,8 +19,8 @@ const AddDog = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const id = uuidv4();
-    dispatch(addDog({ id, name, info, image }));
-    navigate('/'); // Navigate back to the list
+    dispatch(addDog({ id, name, age, gender, color, nickname, owner, image }));
+    navigate('/');
   };
 
   const handleImageChange = (e) => {
@@ -27,24 +31,21 @@ const AddDog = () => {
     <div>
       <h1>Add a Dog</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Dog's Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Information"
-          value={info}
-          onChange={(e) => setInfo(e.target.value)}
-        />
+        <input type="text" placeholder="Dog's Name" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="number" placeholder="Age" value={age} onChange={(e) => setAge(e.target.value)} />
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="">Select Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+        <input type="text" placeholder="Color" value={color} onChange={(e) => setColor(e.target.value)} />
+        <input type="text" placeholder="Nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} />
+        <input type="text" placeholder="Owner's Name" value={owner} onChange={(e) => setOwner(e.target.value)} />
         <input type="file" onChange={handleImageChange} />
-        <button type="submit" className="add-dog-button">Add Dog</button> {/* Apply the CSS class */}
+        <button type="submit">Add Dog</button>
       </form>
     </div>
   );
 };
 
 export default AddDog;
-
--- src/components/DogDetail.js
+++ src/components/DogDetail.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 
 const DogDetail = () => {
   const { id } = useParams();
-  const dog = useSelector((state) => state.dogs.find((dog) => dog.id === id));
+  const dog = useSelector(state => state.dogs.find(dog => dog.id === id));
 
   if (!dog) {
     return <h2>Dog not found</h2>;
@@ -13,8 +13,12 @@ const DogDetail = () => {
   return (
     <div>
       <h1>{dog.name}</h1>
-      <p>{dog.info}</p>
-      {dog.image && <img src={dog.image} alt={dog.name} />}
+      <p>Age: {dog.age}</p> {/* Display age */}
+      <p>Gender: {dog.gender}</p> {/* Display gender */}
+      <p>Color: {dog.color}</p> {/* Display color */}
+      <p>Nickname: {dog.nickname}</p> {/* Display nickname */}
+      <p>Owner: {dog.owner}</p> {/* Display owner's name */}
+      {dog.image && <img src={dog.image} alt={dog.name} style={{ width: "300px", height: "300px" }} />} {/* Display image if available */}
     </div>
   );
 };
