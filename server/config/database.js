167b54b331ed635d0273e67db138a59921a365b0
claudia-nikel <claudia.nikel@gmail.com>
2024-06-30 15:38:05 -0700

    ensuring db password is a string

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); // Load environment variables from .env file
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, String(process.env.DB_PASSWORD), {
   host: process.env.DB_HOST,
   dialect: 'postgres',
   port: process.env.DB_PORT,
